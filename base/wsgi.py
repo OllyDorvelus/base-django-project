363f22ff0d849c185e15b0a72ea5ee86e8241cdd
ollydorvelus <ollydorvelus@gmail.com>
2020-01-02 23:31:17 -0500

    using django configurations for settings and launch

--- base/wsgi.py
+++ base/wsgi.py
@@ -1,16 +1,18 @@
 """
-WSGI config for base project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
-"""
+# WSGI config for base project.
+#
+# It exposes the WSGI callable as a module-level variable named ``application``.
+#
+# For more information on this file, see
+# https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+# """
 
 import os
 
-from django.core.wsgi import get_wsgi_application
+from configurations.wsgi import get_wsgi_application
 
+# APPNAME.SETTINGS
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'base.settings')
+os.environ.setdefault('DJANGO_CONFIGURATION', 'Dev')
 
 application = get_wsgi_application()
